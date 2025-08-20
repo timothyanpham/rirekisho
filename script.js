3d9ed5617b1098f720791818d6d0c05832944fcb
timothyanpham <162242548+timothyanpham@users.noreply.github.com>
2025-08-20 16:47:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// const expContent = document.querySelector("#exp-content");
+// const eduContent = document.querySelector("#edu-content");
+// const activContent = document.querySelector("#activ-content");
+// const favorContent = document.querySelector("#favor-content");
+// const langContent = document.querySelector("#lang-content");
+// const skillContent = document.querySelector("#skill-content");
+
+
+  
